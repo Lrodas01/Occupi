8efd00d56797f62624a933ed95332d09ec0cfe28
Lrodas01 <lrodaroda23@gmail.com>
2024-07-19 09:37:47 -0700

    First Commit for Occupi application

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ngrok = require ('ngrok');
+const app = express()
+
+app.post('/dice', (req, res) => {
+        const randomDice = Math.floor(Math.random()*6) + 1;
+        res.send(`Dice rolled: ${randomDice}`)
+})
+
+app.get ('/test', (req, res)=> {
+    res.send('Hello world');
+});
+
+app.get('/', (req, res) => {
+    res.json({success: true, message: 'Welcome to backend zone!'});
+});
+
+const PORT = process.env.PORT || 3200;
+
+app.listen(PORT, () => {console.log(`server started on port ${PORT}`);
+ngrok.connect(PORT).then(ngrokURL =>{
+    console.log(`Ngrok tunnel in: ${ngrokURL}`)
+}).catch(error => {
+    console.log (`Couldn't tunnel ngrok: ${error}`)
+})
+})
